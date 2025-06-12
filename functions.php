524d1808daee59714131c2b8b624418fed2ccc93
eriko <j.v.xen19@gmail.com>
2025-06-12 15:55:58 +0900

    0612 wp　ファイル修正

--- functions.php
+++ functions.php
@@ -7,6 +7,7 @@ function my_theme_enqueue_assets()
     wp_enqueue_style('base-style', get_template_directory_uri() . '/_css/base.css', array(), filemtime(get_template_directory() . '/_css/base.css'));
     wp_enqueue_style('top-style', get_template_directory_uri() . '/_css/top.css', array(), filemtime(get_template_directory() . '/_css/top.css'));
 
+
     // ----- JavaScript -----
     // ハンバーガーメニュー用のJS（テーマ内に custom.js を設置する想定）
     wp_enqueue_script('custom-script', get_template_directory_uri() . '/js/script.js', array(), filemtime(get_template_directory() . '/js/script.js'), true);
