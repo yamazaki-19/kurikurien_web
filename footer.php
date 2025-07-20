7351d26d938410573040679f5f21bf10b6422e0c
eriko <j.v.xen19@gmail.com>
2025-07-20 13:16:33 +0900

    0719　表示崩れ修正

--- footer.php
+++ footer.php
@@ -16,8 +16,7 @@
 
 <!-- 自作のJS（Splideを使う） -->
 <script src="<?= get_template_directory_uri(); ?>/js/fadein.js"></script>
-<script async defer src="https://www.instagram.com/embed.js"></script>
-
+<script async src="//www.instagram.com/embed.js"></script>
 </body>
 
 </html>
